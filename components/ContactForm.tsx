3294be6f7cf3b8b6578e9b72a83b3c3d9ade0aeb
Muhammad Ahmed <muhammadahmed-lab@github.com>
2026-02-22 20:10:05 +0500

    Update Web3Forms access key

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -7,7 +7,7 @@ export default function ContactForm() {
       <p className="sub">I&apos;ll get back to you within 24 hours.</p>
 
       <form action="https://api.web3forms.com/submit" method="POST">
-        <input type="hidden" name="access_key" value="e8f9f897-f99d-4643-af52-c3c47577b23d" />
+        <input type="hidden" name="access_key" value="669ad038-9c2d-442a-b8b9-cf2137997f39" />
         <input type="hidden" name="subject" value="New message from Portfolio" />
         <input type="hidden" name="redirect" value="false" />
         <div className="form-group">
